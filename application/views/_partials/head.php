1a3d92a0941ab5296389210fefc0f706029398c4
kurniawansgt <kurniawan.sgt@gmail.com>
2020-09-25 09:27:18 +0700

    header & footer update

--- application/views/_partials/head.php
+++ application/views/_partials/head.php
@@ -5,7 +5,7 @@
 <meta name="description" content="">
 <meta name="author" content="">
 
-<title>Administrasi Data Ganda</title>
+<title>SIAP-BSP</title>
 
 <!-- Custom fonts for this template-->
 <link href="<?php echo base_url()?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
